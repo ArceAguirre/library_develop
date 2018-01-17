8ce912c111c057e2d086fdc014960d8b92d87ab2
Arce <arce.aguirre.trevino@gmail.com>
2018-01-17 12:44:46 -0600

    Inicio

--- html/registry.php
+++ html/registry.php
@@ -1,36 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Reportes</title>
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
-    <link rel="styleSheet" type="text/css" href="../css/header.css">
-    <link rel="styleSheet" type="text/css" href="../css/table.css">
+  <title>Reportes</title>
+  <!--  Hojas de estilo -->  
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+  <link rel="styleSheet" type="text/css" href="../css/header.css">
+  <link rel="styleSheet" type="text/css" href="../css/table.css">
+
+  <!-- Meta -->
+  <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Expires" content="0">
+  <meta http-equiv="Last-Modified" content="0">
+  <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  
+  <!-- Java Script -->
 </head>
 <body>
     <!-- Header -->
     <?php include("header/header.php") ?>
     <!-- Insert -->
-    <div class="container" style="padding-top: 120px;">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <h1 align="middle" style="padding-bottom: 10px;">REGISTRO DIARIO</h1>
-                <div class="input-group custom-search-form">
-                <input type="text" class="form-control" placeholder="Código">
-                <span class="input-group-btn">
-                <button class="btn btn-primary" type="button">Registrar</button>
-                </span>
-             </div>
+      <div class="container" style="padding-top: 120px;">
+          <div class="row">
+              <div class="col-md-6 col-md-offset-3 text-center">
+                  <h1 align="middle" >Registro de Libros</h1>
+                  <label >Prestamo por día</label>
+                  <div class="input-group custom-search-form">
+                  <input type="text" class="form-control" placeholder="Código" maxlength="20">
+                  <span class="input-group-btn">
+                  <button class="btn btn-primary" type="button">Registrar</button>
+                  </span>
+               </div>
+          </div>
+      </div>
+    </div>
+  <div class="container">
+     <div class="row">
+        <div class="col-md-6 col-md">
+          <label>Día</label>
         </div>
-    </div>
-    </div>
-
+        <div class="col-md-6 col-md text-right">
+          <label>Total</label>
+        </div>
+      </div>
+  </div>
     <!--Daily report-->
-    <div class="container" style="padding-top: 180px;">
+    <div class="container">
     <div class="row">
         <div class="col-md-12 col-md">
             <div class="panel panel-default ticket-list">
               <div class="panel-heading">
-                <h2 class="panel-title text-center">LIBROS REGISTRADOS</h2>
+                <h2 class="panel-title text-center">Libros Registrados</h2>
               </div>
               <div class="panel-body ">
                   <table class="table">
@@ -56,9 +79,5 @@
         </div>
     </div>
 </div>
-
-
-
-
 </body>
 </html>
